0ef5f7c1f4c5745c742f5958128168c043e1f5c4
darwish <ahmed.darwish@awfarmarket.com>
2022-11-24 02:48:56 +0200

    create history fragment

---
+++ app/src/main/java/com/app/fitness/presenter/history/HistoryFragment.kt
@@ -0,0 +1,10 @@
+package com.app.fitness.presenter.history
+
+import com.app.fitness.common.base.BaseFragment
+import com.app.fitness.databinding.FragmentHistoryBinding
+
+class HistoryFragment : BaseFragment<FragmentHistoryBinding>() {
+    override fun getViewBinding(): FragmentHistoryBinding = FragmentHistoryBinding.inflate(layoutInflater)
+
+
+}
