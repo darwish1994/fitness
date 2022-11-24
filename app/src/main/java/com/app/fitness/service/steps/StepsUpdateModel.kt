d0619e1a0a0cc7c6710b1250192e47e05244ee2b
darwish <ahmed.darwish@awfarmarket.com>
2022-11-24 03:43:33 +0200

    create steps update model

---
+++ app/src/main/java/com/app/fitness/service/steps/StepsUpdateModel.kt
@@ -0,0 +1,41 @@
+package com.app.fitness.service.steps
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+
+class StepsUpdateModel( private val context: Context) :
+    StepsClient {
+    override fun getStepsUpdates(): Flow<Int> {
+        return callbackFlow {
+
+            val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+            val stepsSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+            val stepsCallback = object : SensorEventListener{
+                override fun onSensorChanged(p0: SensorEvent?) {
+                    launch { send(p0?.values?.first()?.toInt()?:0) }
+                }
+
+                override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                }
+            }
+
+            if (stepsSensor == null)
+                throw StepsClient.StepException("No Step Counter Sensor")
+            else
+                sensorManager.registerListener(stepsCallback,stepsSensor,SensorManager.SENSOR_DELAY_UI)
+
+
+            awaitClose {
+                sensorManager.unregisterListener(stepsCallback)
+            }
+
+        }
+    }
+}
