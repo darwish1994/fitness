d3f9929da47ab3a6dc8800bb0ca96554a9cbd747
darwish <ahmed.darwish@awfarmarket.com>
2022-11-25 19:48:41 +0200

    add activity and fragment  view binding delegation

--- app/src/main/java/com/app/fitness/common/extention/Extention.kt
+++ app/src/main/java/com/app/fitness/common/extention/Extention.kt
@@ -3,12 +3,32 @@ package com.app.fitness.common.extention
 import android.app.Notification
 import android.app.NotificationManager
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.viewbinding.ViewBinding
+import com.app.fitness.common.base.FragmentViewBindingDelegate
 
+// observer
 fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, body: (T?) -> Unit) =
     liveData.observe(this, Observer(body))
+
+
+// activity delegation
+inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
+    crossinline bindingInflater: (LayoutInflater) -> T) =
+    lazy(LazyThreadSafetyMode.NONE) {
+        bindingInflater.invoke(layoutInflater)
+    }
+
+// fragment delegation
+fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
+    FragmentViewBindingDelegate(this, viewBindingFactory)
+
 
 fun Notification.stepNotify(context: Context){
     val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
