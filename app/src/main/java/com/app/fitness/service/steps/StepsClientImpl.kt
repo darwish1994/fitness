20d9dd49f579b33501e098721e17a93e712116ed
darwish <ahmed.darwish@awfarmarket.com>
2022-11-25 05:09:18 +0200

    fix bugs of register listener limit

--- app/src/main/java/com/app/fitness/service/steps/StepsClientImpl.kt
+++ app/src/main/java/com/app/fitness/service/steps/StepsClientImpl.kt
@@ -11,16 +11,15 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
 
-class StepsClientImpl(private val context: Context) :
-    StepsClient {
+class StepsClientImpl(private val context: Context) : StepsClient {
+    private var isRuning = false
     override fun getStepsUpdates(): Flow<Int> {
         return callbackFlow {
-
             val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
             val stepsSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-            val stepsCallback = object : SensorEventListener{
+            val stepsCallback = object : SensorEventListener {
                 override fun onSensorChanged(p0: SensorEvent?) {
-                    launch { send(p0?.values?.first()?.toInt()?:0) }
+                    launch { send(p0?.values?.first()?.toInt() ?: 0) }
                 }
 
                 override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
@@ -30,14 +29,21 @@ class StepsClientImpl(private val context: Context) :
             if (stepsSensor == null)
                 throw StepsClient.StepException(context.getString(R.string.no_sensor))
             else {
-               val register= sensorManager.registerListener(
-                    stepsCallback,
-                    stepsSensor,
-                    SensorManager.SENSOR_DELAY_UI
-                )
+                if (!isRuning) {
 
-                if (!register)
-                    throw StepsClient.StepException( context.getString(R.string.fail_to_register_sensor))
+
+                    isRuning = true
+
+                    val register = sensorManager.registerListener(
+                        stepsCallback,
+                        stepsSensor,
+                        SensorManager.SENSOR_DELAY_FASTEST
+                    )
+
+                    if (!register)
+                        throw StepsClient.StepException(context.getString(R.string.fail_to_register_sensor))
+                }
+
             }
 
             awaitClose {
