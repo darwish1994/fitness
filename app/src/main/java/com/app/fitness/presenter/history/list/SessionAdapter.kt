75767a681fae1167659b1dfe8a8e21e5626b5666
darwish <ahmed.darwish@awfarmarket.com>
2022-11-26 00:39:55 +0200

    create history adapter

---
+++ app/src/main/java/com/app/fitness/presenter/history/list/SessionAdapter.kt
@@ -0,0 +1,55 @@
+package com.app.fitness.presenter.history.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.app.fitness.common.diff.SessionDiffUtil
+import com.app.fitness.common.extention.getDistanceCovered
+import com.app.fitness.common.extention.timerFormat
+import com.app.fitness.databinding.ItemLayoutSessionBinding
+import com.app.fitness.domain.model.Session
+
+class SessionAdapter (private val listener:(Session)->Unit): RecyclerView.Adapter<SessionAdapter.SessionViewHolder>() {
+
+    private val dataList = arrayListOf<Session>()
+
+    fun updateList(data: List<Session>) {
+        val diffUtil = SessionDiffUtil(dataList, data)
+        val diffResults = DiffUtil.calculateDiff(diffUtil)
+        diffResults.dispatchUpdatesTo(this)
+        dataList.apply {
+            clear()
+            addAll(data)
+        }
+    }
+
+
+    inner class SessionViewHolder(private val layout: ItemLayoutSessionBinding) :
+        RecyclerView.ViewHolder(layout.root) {
+
+        init {
+            layout.root.setOnClickListener {
+                listener.invoke(dataList[adapterPosition])
+            }
+        }
+
+        fun binding(item: Session) {
+            layout.tvDistance.text = item.steps?.getDistanceCovered()
+            layout.tvSteps.text = item.steps.toString()
+            layout.tvDuration.text = item.duration?.timerFormat()
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SessionViewHolder =
+        SessionViewHolder(
+            ItemLayoutSessionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+
+    override fun onBindViewHolder(holder: SessionViewHolder, position: Int) {
+        holder.binding(dataList[position])
+    }
+
+    override fun getItemCount(): Int = dataList.size
+}
