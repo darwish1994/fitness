31b87afcc7c87327ec94e373c214610d4da727ee
darwish <ahmed.darwish@awfarmarket.com>
2022-11-26 00:41:22 +0200

    update with time extention function

--- app/src/main/java/com/app/fitness/presenter/history/HistoryFragment.kt
+++ app/src/main/java/com/app/fitness/presenter/history/HistoryFragment.kt
@@ -1,10 +1,42 @@
 package com.app.fitness.presenter.history
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.app.fitness.R
+import com.app.fitness.common.extention.observe
+import com.app.fitness.common.extention.viewBinding
+import com.app.fitness.databinding.FragmentHistoryBinding
+import com.app.fitness.domain.model.Session
+import com.app.fitness.presenter.history.list.SessionAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
-class HistoryFragment : Fragment(R.layout.fragment_history){
+@AndroidEntryPoint
+class HistoryFragment : Fragment(R.layout.fragment_history) {
+    private val binding by viewBinding(FragmentHistoryBinding::bind)
+
+    private val viewModel by viewModels<HistoryViewModel>()
+
+    private val adapter by lazy {
+        SessionAdapter {
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recSession.adapter = adapter
+        if (!viewModel.sessionsLiveData.hasActiveObservers())
+            observe(viewModel.sessionsLiveData, ::sessionObserver)
+        viewModel.getSessions()
 
 
+    }
+
+
+    private fun sessionObserver(it: List<Session>?) {
+        adapter.updateList(it ?: arrayListOf())
+    }
 
 }
