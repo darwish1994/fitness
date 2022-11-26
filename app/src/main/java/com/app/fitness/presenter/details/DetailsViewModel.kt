007412ce1bf720d71a47d68ffa788a1bb527d1e1
darwish <ahmed.darwish@awfarmarket.com>
2022-11-26 03:44:43 +0200

    create details view

---
+++ app/src/main/java/com/app/fitness/presenter/details/DetailsViewModel.kt
@@ -0,0 +1,21 @@
+package com.app.fitness.presenter.details
+
+import androidx.lifecycle.ViewModel
+import com.app.fitness.data.local.SessionTracking
+import com.app.fitness.domain.usecase.SessionDetailsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsViewModel @Inject constructor(
+    private val sessionDetailsUseCase: SessionDetailsUseCase
+) : ViewModel() {
+
+
+    suspend fun getSession(id: Int): SessionTracking =
+        withContext(Dispatchers.IO) {
+            sessionDetailsUseCase.invoke(id)
+        }
+}
